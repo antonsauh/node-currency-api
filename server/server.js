4e123ab710ce1345b36d1b97664aca1952afcc37
Anton Sauh <AntonSauh@Antons-MacBook-Pro.local>
2017-10-26 13:53:21 +0300

    Working on data verif methods and bugfixes, added currency types available, hardcoded for now

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 require('./config/config');
 
 const express = require('express');
@@ -17,91 +16,108 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// list of supported currencies
+const currencies = ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR",
+    "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR"];
+
 //returns an object containing info for rates on currency. Also writes results into the Database if they do not exist already.
 // if results already exist, then data is recieved from db and appended to the response data array.
 app.get('/rates', (req, res) => {
-    // declaring initial variables
-    let newData = []; // end data array sent to the app
-    let date = new Date(); // declaring new date on each request to return data for today and 25 weeks before.
-    let i = 0; // data
-    const base = req.header("base");
-    const rates = req.header("rates");
 
-    async.whilst(function () { return i <= 25}, function (callback) {
+    // checking if request is OK
 
-        let ts = date.getTime();
-        let minusDays = ts - ((7 * i) * 24 * 60 * 60 * 1000);
-        let minusDate = new Date (minusDays);
-        let dateObj = {
-            'year': minusDate.getFullYear().toString(),
-            'month': (minusDate.getMonth() + 1).toString(),
-            'date': (minusDate.getDate() - 1).toString()
-        };
+    if (req.header("base") === req.header("rates")) {
+        res.status(400).send("Bad Request");
+    } else if (currencies.indexOf(req.header("base")) === -1 || currencies.indexOf(req.header("rates")) === -1) {
+        res.status(400).send("Bad Request");
+    }
 
-        if (dateObj.date.length !== 2 && dateObj.month.length !== 2) {
-            dateObj.date = "0" + dateObj.date;
-            dateObj.month = "0" + dateObj.month;
-        } else if (dateObj.date.length !== 2) {
-            dateObj.date = "0" + dateObj.date;
-        } else if (dateObj.month.length !== 2) {
-            dateObj.month = "0" + dateObj.month;
-        }
 
-        if(dateObj.date === "00") {
-            dateObj.date = "01";
-        }
+    else {
 
-        i++;
+        // declaring initial variables
+        let newData = []; // end data array sent to the app
+        let date = new Date(); // declaring new date on each request to return data for today and 25 weeks before.
+        let i = 0; // data
+        const base = req.header("base");
+        const rates = req.header("rates");
+        async.whilst(function () {
+                return i <= 25
+            }, function (callback) {
 
-        let reqDate = `${dateObj.year}-${dateObj.month}-${dateObj.date}?base=${base}`;
-        // query database, before making http request to API.
-        Record.find({base: base, date: `${dateObj.year}-${dateObj.month}-${dateObj.date}`}).then((data) => {
-            if (data.length >= 1) {
-                newData.push({
-                    base: data[0].base,
-                    date: data[0].date,
-                    rates: data[0].rates[rates]
-                });
-                callback(null, newData);
-            } else {
-                http.get('http://api.fixer.io/' + reqDate, (resp) => {
-                    let data = [];
-                    resp.on('data', (chunk) => {
-                        data += chunk;
-                    });
+                let ts = date.getTime();
+                let minusDays = ts - ((7 * i) * 24 * 60 * 60 * 1000);
+                let minusDate = new Date(minusDays);
+                let dateObj = {
+                    'year': minusDate.getFullYear().toString(),
+                    'month': (minusDate.getMonth() + 1).toString(),
+                    'date': (minusDate.getDate() - 1).toString()
+                };
 
-                    resp.on('end', () => {
-                        let newRecord = new Record(JSON.parse(data));
+                if (dateObj.date.length !== 2 && dateObj.month.length !== 2) {
+                    dateObj.date = "0" + dateObj.date;
+                    dateObj.month = "0" + dateObj.month;
+                } else if (dateObj.date.length !== 2) {
+                    dateObj.date = "0" + dateObj.date;
+                } else if (dateObj.month.length !== 2) {
+                    dateObj.month = "0" + dateObj.month;
+                }
+
+                if (dateObj.date === "00") {
+                    dateObj.date = "01";
+                }
+
+                i++;
+
+                let reqDate = `${dateObj.year}-${dateObj.month}-${dateObj.date}?base=${base}`;
+                // query database, before making http request to API.
+                Record.find({base: base, date: `${dateObj.year}-${dateObj.month}-${dateObj.date}`}).then((data) => {
+                    if (data.length >= 1) {
                         newData.push({
-                            base: newRecord.base,
-                            date: newRecord.date,
-                            rates: newRecord.rates[rates]
+                            base: data[0].base,
+                            date: data[0].date,
+                            rates: data[0].rates[rates]
                         });
-                        newRecord.save();
                         callback(null, newData);
+                    } else {
+                        http.get('http://api.fixer.io/' + reqDate, (resp) => {
+                            let data = [];
+                            resp.on('data', (chunk) => {
+                                data += chunk;
+                            });
 
-                    });
-                }).on('error', (err) => {
-                    console.log("Error: " + err.message);
-                    callback(err);
-                });
+                            resp.on('end', () => {
+                                let newRecord = new Record(JSON.parse(data));
+                                newData.push({
+                                    base: newRecord.base,
+                                    date: newRecord.date,
+                                    rates: newRecord.rates[rates]
+                                });
+                                newRecord.save();
+                                callback(null, newData);
 
-            }
+                            });
+                        }).on('error', (err) => {
+                            console.log("Error: " + err.message);
+                            callback(err);
+                        });
 
-        }).catch((e) => {
-            res.send(e);
-            console.log("ERROR");
-        })
-    },
-    function(err, newData) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(newData);
-        }
-    });
+                    }
 
+                }).catch((e) => {
+                    res.send(e);
+                    console.log("ERROR");
+                })
+            },
+            function (err, newData) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(newData);
+                }
+            });
 
+    }
 });
 
 app.listen(process.env.PORT, () => {
